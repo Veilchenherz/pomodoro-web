345e577f893ca1d721d3a24ac15243d68927d9b7
Veilchen <laurin.homberg@outlook.de>
2024-12-21 19:24:42 +0100

    scripts added

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let workingDuration = 25;
-let shortBreakDuration = 5;
-let longBreakDuration = 20;
-let timerText = workingDuration + ":00"
+let working = 25;
+let timerText = working + ":00";
 
-document.getElementById("work-input").setAttribute("value", workingDuration);
-document.getElementById("short-input").setAttribute("value", shortBreakDuration);
-document.getElementById("long-input").setAttribute("value", longBreakDuration);
 document.getElementById("timer").innerText = timerText;
---
+++ settings.js
@@ -0,0 +1,7 @@
+let workingDuration = 25;
+let shortBreakDuration = 5;
+let longBreakDuration = 20;
+
+document.getElementById("work-input").setAttribute("value", workingDuration);
+document.getElementById("short-input").setAttribute("value", shortBreakDuration);
+document.getElementById("long-input").setAttribute("value", longBreakDuration);
