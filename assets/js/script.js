a16a2a5be05596bed6a6774cd8844d038ea9967b
Laurin Homberg <laurin.homberg@outlook.de>
2024-12-27 14:05:52 +0100

    added functionality to start and reset buttons for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ let workingDuration = 25;
 let shortBreakDuration = 5;
 let longBreakDuration = 20;
 
-var timer = 0;
+let timerID = null;
 
 
 // takes minutes and seconds as input and displays those on the screen 
-// with correct formatting (0 added infront, if number of digits below 2)
+// with correct formatting (0 added in front, if number of digits below 2)
 function displayTimer(minutes, seconds) {
 
     let secondsDigits = Math.floor(Math.log10(seconds)) + 1;
@@ -29,37 +29,39 @@ function displayTimer(minutes, seconds) {
 // takes the number of repetitions as an input
 // displays the current action the user is supposed to do 
 // (work, short break, long break)
+// displays "Pomodoro", if repetitions is equal to zero (used while timer is not running)
 function displayAction (repetitions) {
 
-    if (repetitions === 0) {
-        document.getElementById("heading").innerText = "Pomodoro";
-    }
-    else {
-        switch (0) {
+    switch (0) {
 
-            case (repetitions % 8): {
-                document.getElementById("heading").innerText = "Long Break";
-                break;
-            }
-    
-            case (repetitions % 2): {
-                document.getElementById("heading").innerText = "Short Break";
-                break;
-            }
-    
-            default: {
-                document.getElementById("heading").innerText = "Work";
-            }
-    
+        case (repetitions): {
+            document.getElementById("heading").innerText = "Pomodoro";
+            break;
         }
+
+        case (repetitions % 8): {
+            document.getElementById("heading").innerText = "Long Break";
+            break;
+        }
+
+        case (repetitions % 2): {
+            document.getElementById("heading").innerText = "Short Break";
+            break;
+        }
+
+        default: {
+            document.getElementById("heading").innerText = "Work";
+        }
+
     }
 }
 
 
-//takes time in seconds as input and counts down from the given time till zero
+
+//takes time in seconds as input and counts down from the given time to zero
 function pomodoroTimer (duration) {
 
-    timer = setInterval(function timeStep() {
+    timerID = setInterval(function timeStep() {
         let secondsLeft = duration % 60;
         let minutesLeft = Math.floor(duration / 60);
 
@@ -69,19 +71,20 @@ function pomodoroTimer (duration) {
         duration--;
 
         if (duration < 0) {
-            clearInterval(timer);
+            clearInterval(timerID);
         }
     }, 1000);
 }
 
-function resetTimer (timerToReset) {
-    if (timerToReset !== 0) {
-        clearInterval(timer);
+function resetTimer () {
+    if (!timerID) {
+        clearInterval(timerID);
     }
 }
 
 
-//document.getElementById("start").addEventListener("click", pomodoroTimer.bind(null, 180))
-//document.getElementById("reset").addEventListener("click", clearInterval.bind(timer))
-
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("start").addEventListener("click", () => pomodoroTimer(workingDuration * 60));
+    document.getElementById("reset").addEventListener("click", () => resetTimer());
+});
 
