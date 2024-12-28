4dd154c06fe252bf492fbd8cd2626c85d933da3e
Veilchen <laurin.homberg@outlook.de>
2024-12-28 20:41:19 +0100

    Update comments in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,7 @@ function displayAction (actionText) {
 
 
 // takes time in seconds as input and counts down from the given time to zero
+// returns false as soon as the timer reached zero
 async function pomodoroTimer (duration) {
 
     return new Promise((resolve) => {
